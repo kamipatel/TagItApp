88efeb371ec57ab48eb54a45011b91f642a283b1
kamipatel@gmail.com <kamipatel@gmail.com>
2021-10-22 15:25:18 -0500

    push

--- force-app/main/default/lwc/pageTrackerCmp/pageTrackerCmp.js
+++ force-app/main/default/lwc/pageTrackerCmp/pageTrackerCmp.js
@@ -1,3 +1,29 @@
 import { LightningElement } from 'lwc';
+import getSomething from '@salesforce/apex/TagHelper.getSomething';
 
-export default class PageTrackerCmp extends LightningElement {}
+export default class PageTrackerCmp extends LightningElement {
+
+    handleClick(event) {
+
+        console.log("handleClick called");
+
+        //event.preventDefault();
+
+        getSomething({
+            data: "FAIL"
+        }).then((data) => {            
+            console.log("getSomething msg=" + msg);
+        });    
+        /*
+        getSomething({
+            data: "FAIL"
+        })
+        .then((data) => {            
+            console.log("getSomething msg=" + msg);
+        })
+        .catch((error) => {
+            this.message = 'getSomething Error received: code' + error.errorCode + ', ' +
+                'message ' + error.body.message;                
+        });              */
+    }
+}
